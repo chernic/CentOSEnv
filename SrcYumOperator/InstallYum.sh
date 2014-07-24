3a89a8afd7714b5aa10ae9f03d51c1cb6ebb4ea5
chernic <iamchernic@gmail.com>
2014-07-24 18:05:12 +0800

    Install Yum Update
    # v0.0.4(2014-7-24) : Add Aliyum Mirror

--- SrcYumOperator/InstallYum.sh
+++ SrcYumOperator/InstallYum.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #####################################################
-# Version : 0.0.2
+# Fuction of yum install
+#####################################################
+# Version : 0.0.6
 # Make by Chernic.Y.Chen @ China
 # E-Mail : iamchernic@gmail.com
-# Date : 2014-6-12
+# Date : 2014-7-22
 # v0.0.2(2014-7-18) : Add NotRootOut()
 # v0.0.3(2014-7-18) : Add Dir
+# v0.0.4(2014-7-24) : Add Aliyum Mirror
 #####################################################
-# This Shell Script has been Test on RHEL-5Server
-#####################################################
-# 获取本文件所在路径
-BUILD_DIR=`pwd`
-# Add Debug Flag
 export PS4='+[$LINENO]'
 AUTO_FLAG_yn="n"
 BreakPoint()
@@ -27,6 +25,26 @@ NotRootOut()
 {
 	[ $(id -u) != "0" ] && echo "Error: You must be root to run this script" && exit 1 
 }
+
+GetIPAddress()
+{
+	IPAddress=`ifconfig eth0 | grep 'inet addr' | cut -d ":" -f 2 | cut -d " " -f 1`
+}
+
+ReadConf()
+{
+	# [获取本文件路径中 的 文件名称] 即本文件名的配置文件
+	CONF_FILE=$(basename $0 .sh).conf
+	# 判断对应文件是否存在,若存在则导入
+	[ -f $CONF_FILE ] && . $CONF_FILE
+}
+############### Template Version 0.0.6 ##############
+# 获取本文件所在路径
+BUILD_DIR=`pwd`
+
+
+
+
 
 ###########################################
 # 检查
@@ -100,28 +118,75 @@ InstallNewRepo()
 
 ###########################################
 # 配置reps
+# RHEL 须直接标明版本
+# 不使用包检查
 ###########################################
+AliyunRepo()
+{
+	if [ -n "$(cat /etc/redhat-release | grep '6\.')" ];then
+		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
+	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
+			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+			sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS-Base.repo
+	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
+		fi
+	elif [ -n "$(cat /etc/redhat-release | grep '5\.')" ];then
+		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
+	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
+			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-5.repo
+			sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS-Base.repo
+	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
+		fi
+	fi
+}
+Sina163Repo()
+{
+	if [ -n "$(cat /etc/redhat-release | grep '6\.')" ];then
+		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
+	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
+			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS6-Base-163.repo
+			sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS-Base.repo
+	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
+		fi
+	elif [ -n "$(cat /etc/redhat-release | grep '5\.')" ];then
+		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
+	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
+			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS5-Base-163.repo
+			sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS-Base.repo
+	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
+		fi
+	fi
+}
+
 ManualRepo()
 {
 
 	echo "************** Get all new repoes"
 	# CentOS - [Necessary]
 	cd $BUILD_DIR/Repos
-	echo "******* Manually Copy 163.repo"
-	cp CentOS5-Base-163.repo          /etc/yum.repos.d/
-	# # # CentOS - Copy From Other System
-	# # cp CentOS-Base.repo               /etc/yum.repos.d/
-	# # cp CentOS-Debuginfo.repo          /etc/yum.repos.d/
-	# # cp CentOS-Media.repo              /etc/yum.repos.d/
-	# # cp CentOS-Vault.repo              /etc/yum.repos.d/
+	echo "******* Add XXX.repo"
+	MIRROR=sina
+	case $MIRROR in
+		aliyun)
+			AliyunRepo;
+		;;
+
+		sina)
+			Sina163Repo;
+		;;
+		
+		localaliyun)
+			cp CentOS5-Base-163.repo   /etc/yum.repos.d/
+		;; 
+		
+		localsina)
+			cp CentOS5-aliyun.repo     /etc/yum.repos.d/
+		;; 
+		*)
+		;;
+	esac 
 	echo "******* EPEL.rpm had Helped us to Add Repos"
-	# cp epel.repo                    /etc/yum.repos.d/
-	# cp epel-testing.repo            /etc/yum.repos.d/
 	echo "******* rpmforge.rpm had Helped us to Add Repos"
-	# cp rpmforge.repo                /etc/yum.repos.d/
-	# cp mirrors-rpmforge             /etc/yum.repos.d/
-	# cp mirrors-rpmforge-extras      /etc/yum.repos.d/
-	# cp mirrors-rpmforge-testing     /etc/yum.repos.d/
 	#################
 	# 人工优化配置Reps
 	#################
@@ -136,10 +201,19 @@ ManualRepo()
 YumTest()
 {
 	# 查看所有Repoes
+
 	echo " // Find all Repoes"
 	ls /etc/yum.repos.d/
+	
+	# 命令将服务器上的 软件包 信息 现在本地缓存，以提高 搜索 安装软件的速度
+	echo " // MakeCaches"
+	yum makecache	
 	echo " // List all Repoes"
 	yum repolist all
+	
+	# 风速慢的话可以通过增加yum的超时时间，这样就不会总是因为超时而退出。 
+	# vi /etc/yum.conf 
+	# 加上这么一句：timeout=120 	
 }
 
 NotRootOut;
@@ -157,20 +231,3 @@ ManualRepo;
 
 # 测试
 YumTest;
-
-
-# # 命令将服务器上的 软件包 信息 现在本地缓存，以提高 搜索 安装软件的速度
-# echo "******* Make Cache"
-# yum makecache
-
-# 风速慢的话可以通过增加yum的超时时间，这样就不会总是因为超时而退出。 
-# vi /etc/yum.conf 
-# 加上这么一句：timeout=120 
-
-# [clusterlabs-next]
-# name=High Availability/Clustering server technologies (epel-5-next)
-# baseurl=http://www.clusterlabs.org/rpm-next/epel-5
-# metadata_expire=45m
-# type=rpm-md
-# gpgcheck=0
-# enabled=1
