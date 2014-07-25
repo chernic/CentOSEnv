475782ef0ac14f80cfe4c540a39285598086db9b
chernic <iamchernic@gmail.com>
2014-07-25 13:51:03 +0800

    Install Yum Update
    Functional, Template, Testable

--- SrcYumOperator/InstallYum.sh
+++ SrcYumOperator/InstallYum.sh
@@ -58,14 +58,14 @@ CheckRpms()
 	echo -e "yum-rhn-plugin-0.5.4-22.el5"
 	echo -e "yum-updatesd-0.9-2.el5\n"
 	
-	echo "What's in This System :"
-	echo "Wait 1"
+	echo " // What's in This System :"
+	echo "  Grep yum"
 	rpm -aq|grep yum;
-	echo "Wait 2"
+	echo " Grep python-iniparse"
 	rpm -aq|grep python-iniparse;
-	echo "Wait 3"
+	echo "  Grep epel-release"
 	rpm -aq|grep epel-release;
-	echo "Wait 4"
+	echo "  Grep rpmforge-release"
 	rpm -aq|grep rpmforge-release;
 }
 
@@ -76,20 +76,22 @@ CheckRpms()
 RemoveOldRpms()
 {
 	BreakPoint;
-	echo "Wait 1"
+	echo "  Uninstall yum... Wait 1"
 	rpm -aq|grep yum|xargs rpm -e --nodeps
-	echo "Wait 2"
+	echo "  Uninstall python-iniparse... Wait 2"
 	rpm -aq|grep python-iniparse|xargs rpm -e --nodeps
-	echo "Wait 3"
+	echo "  Uninstall epel-release... Wait 3"
 	rpm -aq|grep epel-release|xargs rpm -e --nodeps
-	echo "Wait 4"
+	echo "  Uninstall rpmforge-release... Wait 4"
 	rpm -aq|grep rpmforge-release|xargs rpm -e --nodeps
 }
 RemoveOldRepo()
 {
-	BreakPoint;
-	echo "remove all old repoes. Because you know how to Make it."
+	echo "What's in /etc/yum.repos.d/ :"
 	ls /etc/yum.repos.d/; 
+	echo " !!! remove all old repoes. Because you knew how to Make it."	
+	
+	BreakPoint;
 	rm /etc/yum.repos.d/* -f
 }
 
@@ -121,72 +123,72 @@ InstallNewRepo()
 # RHEL 须直接标明版本
 # 不使用包检查
 ###########################################
-AliyunRepo()
+
+CentOsAliyunRepo()
 {
 	if [ -n "$(cat /etc/redhat-release | grep '6\.')" ];then
-		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
-	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
-			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
-			sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS-Base.repo
-	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
-		fi
+		# Aliyun
+		wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+		sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS-Base.repo
+		sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo	
 	elif [ -n "$(cat /etc/redhat-release | grep '5\.')" ];then
-		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
-	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
-			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-5.repo
-			sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS-Base.repo
-	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
-		fi
+		# Aliyun
+		wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-5.repo
+		sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS-Base.repo
+		sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo	
 	fi
 }
-Sina163Repo()
+CentOs163Repo()
 {
 	if [ -n "$(cat /etc/redhat-release | grep '6\.')" ];then
-		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
-	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
-			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS6-Base-163.repo
-			sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS-Base.repo
-	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
-		fi
+		wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS6-Base-163.repo
+		sed -i 's@\$releasever@6@g' /etc/yum.repos.d/CentOS-Base.repo
+		sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
+		# Sina163	
 	elif [ -n "$(cat /etc/redhat-release | grep '5\.')" ];then
-		if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
-	        	/bin/mv /etc/yum.repos.d/CentOS-Base.repo{,_bk}
-			wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS5-Base-163.repo
-			sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS-Base.repo
-	                sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
-		fi
+		wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS5-Base-163.repo
+		sed -i 's@\$releasever@5@g' /etc/yum.repos.d/CentOS-Base.repo
+		sed -i 's@gpgcheck=1@gpgcheck=0@g' /etc/yum.repos.d/CentOS-Base.repo
+		# Sina163
 	fi
+}
+
+DisableRepo()
+{
+	# 找出指定行并修改
+	echo " // Change $1.repo"
+	FILE=$REPO_DIR/$1.repo
+	lss=$1
+	ss=$(grep -n '^\['$lss'\]' $FILE | cut -d ':' -f 1)
+	sed -i  $ss',/enabled/ s/enabled.*/enabled=0/' $FILE
+	grep 'enabled' $FILE
 }
 
 ManualRepo()
 {
-
-	echo "************** Get all new repoes"
-	# CentOS - [Necessary]
-	cd $BUILD_DIR/Repos
-	echo "******* Add XXX.repo"
-	MIRROR=sina
-	case $MIRROR in
-		aliyun)
-			AliyunRepo;
-		;;
-
-		sina)
-			Sina163Repo;
-		;;
-		
-		localaliyun)
-			cp CentOS5-Base-163.repo   /etc/yum.repos.d/
-		;; 
-		
-		localsina)
-			cp CentOS5-aliyun.repo     /etc/yum.repos.d/
-		;; 
-		*)
-		;;
-	esac 
 	echo "******* EPEL.rpm had Helped us to Add Repos"
-	echo "******* rpmforge.rpm had Helped us to Add Repos"
+	echo "******* rpmforge.rpm had Helped us to Add Repos"	
+	
+	cd $BUILD_DIR/Repos	
+	echo "******* Add XXX.repo"
+
+	MIRROR=local
+	BASE=Sina163	
+	if [ ! -z "$(cat /etc/redhat-release | grep 'Red Hat')" ];then
+		MIRROR=local
+	fi
+	if [ "$BASE" == "Sina163" ] && [ "$MIRROR" == "web" ] ;then
+		CentOs163Repo;
+	elif [ "$BASE" == "Alinyun" ] && [ "$MIRROR" == "web" ] ;then
+		CentOsAliyunRepo;
+	elif [ "$BASE" == "Sina163" ] && [ "$MIRROR" == "local" ] ;then
+		cp CentOS5-Base-163.repo   		/etc/yum.repos.d/
+	elif [ "$BASE" == "Alinyun" ] && [ "$MIRROR" == "local" ] ;then
+		cp CentOS5-Base-aliyun.repo     /etc/yum.repos.d/
+	else
+		exit 0
+	fi
+	
 	#################
 	# 人工优化配置Reps
 	#################
@@ -194,6 +196,13 @@ ManualRepo()
 	# cp RHEL-rpmforge-CCmirrors.repo /etc/yum.repos.d/
 	# cp RHEL-rpmforge-CCmirrors      /etc/yum.repos.d/
 }
+
+
+ManulPython24()
+{
+	sed -i 's@#!/usr/bin/python@#!/usr/bin/python2.4@g' /usr/bin/yum  
+}
+
 
 ###########################################
 # 测试
@@ -228,6 +237,7 @@ RemoveOldRepo;
 InstallNewYum;
 InstallNewRepo;
 ManualRepo;
+ManulPython24;
 
 # 测试
 YumTest;
